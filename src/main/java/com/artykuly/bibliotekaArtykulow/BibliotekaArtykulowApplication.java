d2e966dd550a1c0057b5f6064adf6984112bae12
Maniek1337 <55005065+Maniek1337@users.noreply.github.com>
2022-07-19 16:45:58 +0200

    Initial commit

---
+++ src/main/java/com/artykuly/bibliotekaArtykulow/BibliotekaArtykulowApplication.java
@@ -0,0 +1,13 @@
+package com.artykuly.bibliotekaArtykulow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekaArtykulowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotekaArtykulowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artykuly/bibliotekaArtykulow/BibliotekaArtykulowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artykuly.bibliotekaArtykulow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaArtykulowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
